58d4c65927820e781fb1e2c92e5cf3df459e5e02
monica1123 <shelly1123@hotmail.com>
2021-03-13 15:28:33 +0800

    Initial Commit

---
+++ KKTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KKTV
+//
+//  Created by Monica Lo on 2021/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
